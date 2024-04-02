d74ddc8f4d621b25b4cec4e55b400ac3b2b648ee
Yusuf Jima <123868833+YusufJima@users.noreply.github.com>
2024-04-03 00:46:41 +0300

    First Commit

---
+++ MachNumber.py
@@ -0,0 +1,27 @@
+def CalculateMachNumber(Speedms):
+    SpeedofSound = 343
+    MachNumber = Speedms / SpeedofSound
+    return MachNumber
+
+
+def Clasificationofmachnumber(MachNumber):
+    if MachNumber >= 10 and MachNumber <= 25:
+        return "High Hypersonic"
+    elif MachNumber >= 5 and MachNumber < 10:
+        return "Hypersonic"
+    elif MachNumber >= 1.2 and MachNumber < 5:
+        return "Supersonic"
+    elif MachNumber >= 0.8 and MachNumber < 1.2:
+        return "Transonic"
+    else:
+        return "Subsonic"
+
+
+Speedms = float(
+    input("Enter the speed of the aircraft in meters per second (m/s): "))
+MachNumber = CalculateMachNumber(Speedms)
+Clasificacion = Clasificationofmachnumber(MachNumber)
+mensaje = f""" 
+The Mach number is: {round(MachNumber,2)}
+The Clasification is: {Clasificacion} """
+print(mensaje)
